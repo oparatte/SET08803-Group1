ebaafa23ada28f6db9e6f3f660d42dc7499f5c3e
Olivier Paratte <oparatte@gmail.com>
2022-02-26 09:39:42 +0100

    Added Use Case 12

---
+++ src/main/java/com/napier/sem/usecases/UseCase12.java
@@ -0,0 +1,41 @@
+package com.napier.sem.usecases;
+
+import com.napier.sem.dao.City;
+import com.napier.sem.utils.DBUtils;
+
+import java.util.ArrayList;
+
+public class UseCase12 {
+
+    /**
+     * Generates a report with all the top N populated cities in the world,
+     * organised by largest population to smallest
+     */
+    public void printCityPopulationReportDESC(){
+        //Get an instance of the DB Utilities
+        DBUtils dbUtils = new DBUtils();
+        //Connect to the Database;
+        dbUtils.connect();
+
+        //Prepare the SQL Query for the Use Case
+        String query = "SELECT city.*, country.Name as Country FROM city INNER JOIN country ON city.CountryCode=country.Code "
+                + "ORDER BY city.population DESC "
+                + "LIMIT 10 ";
+
+        //Put the result of the query into the Data Object Array
+        ArrayList<City> cities = dbUtils.populateCityArrayList(dbUtils.getResultSet(query));
+
+        //Generate the Report
+        System.out.println("UC 12 Report on top 10 populated Cities in the world");
+        System.out.println(String.format("%-30s %-30s %-30s %-15s","Name","Country","District","Population"));
+        for (City city : cities)
+        {
+            String record = String.format("%-30s %-30s %-30s %-15s",city.getName(),city.getCountry(),
+                    city.getDistrict(),city.getPopulation());
+            System.out.println(record);
+        }
+        //Disconnect from the Database
+        dbUtils.disconnect();
+    }
+
+}
