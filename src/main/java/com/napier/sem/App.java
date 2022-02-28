25a457591b7dc9071ac78b698fa767d0fdfe9d51
Greg Morris <40498429@live.napier.ac.uk>
2022-02-28 00:14:07 -0400

    Create new Feature branch for Use case 20

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,13 +3,17 @@ package com.napier.sem;
 import com.napier.sem.usecases.UseCase1;
 import com.napier.sem.usecases.UseCase2;
 import com.napier.sem.usecases.UseCase3;
-import com.napier.sem.usecases.UseCase41;
+import com.napier.sem.usecases.UseCase20;
 
 
 public class App
 {
 
     public static void main(String[] args) {
+
+        //Execute UseCase 20
+        UseCase20 useCase20 = new UseCase20();
+        useCase20.printPopulationByContinentDESC("Europe", 5);
 
         //Execute UseCase 1
         UseCase1 useCase1 = new UseCase1();
@@ -22,10 +26,6 @@ public class App
         //Execute UseCase 3
         UseCase3 useCase3 = new UseCase3();
         useCase3.printCountryPopulationReportDESC();
-
-        //Execute UseCase 41
-        UseCase41 useCase41 = new UseCase41();
-        useCase41.printWorldPopulation();
 
     }
 
