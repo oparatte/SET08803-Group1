8d085feb582de39d609744833ca500f1b298039a
Greg Morris <77654355+gamorris246@users.noreply.github.com>
2022-03-05 17:31:04 -0400

    Update UseCase9.java

--- src/main/java/com/napier/sem/usecases/UseCase9.java
+++ src/main/java/com/napier/sem/usecases/UseCase9.java
@@ -1,45 +1,41 @@
 package com.napier.sem.usecases;
 
 import com.napier.sem.dao.City;
-import com.napier.sem.dao.Country;
 import com.napier.sem.utils.DBUtils;
 
 import java.util.ArrayList;
 
 public class UseCase9 {
 
-
     /**
-     * Generates a report with all the countries in a region,
+     * Generates a report with all the cities in a region,
      * organised by largest population to smallest
      */
-    public void printCountryPopulationReportDESC(String Region){
+    public void printCityPopulationReportDESC(String Region){
         //Get an instance of the DB Utilities
         DBUtils dbUtils = new DBUtils();
         //Connect to the Database;
         dbUtils.connect();
 
         //Prepare the SQL Query for the Use Case
-        String query = "SELECT * FROM country INNER JOIN city ON country.Capital=city.ID "
-                + "WHERE country.Region = '"+Region+"'"
-                + "ORDER BY country.population DESC";
+        String query = "SELECT city.*, country.Name as Country FROM city INNER JOIN country ON city.CountryCode=country.Code "
+                + "WHERE region.Name = '"+Region+"'"
+                + " ORDER BY city.population DESC";
 
         //Put the result of the query into the Data Object Array
         ArrayList<City> cities = dbUtils.populateCityArrayList(dbUtils.getResultSet(query));
 
         //Generate the Report
-        System.out.println("UC 9 Report on Cities in a Region organised by Population descending");
-        System.out.println(String.format("%-15s %-30s %-15s","City","Population","Capital"));
-        for (City con : cities)
+        System.out.println("UC 9 Report on cities in "+Region+" by population");
+        System.out.println(String.format("%-30s %-30s %-30s %-15s","Name","Region","District","Population"));
+        for (City city : cities)
         {
-            String record = String.format("%-15s %-30s %-15s",con.getName(),con.getPopulation(),
-                    con.getCountry());
+            String record = String.format("%-30s %-30s %-30s %-15s",city.getName(),city.getRegion(),
+                    city.getDistrict(),city.getPopulation());
             System.out.println(record);
         }
         //Disconnect from the Database
         dbUtils.disconnect();
     }
-
-
 
 }
