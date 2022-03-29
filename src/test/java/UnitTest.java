9869910f2f9deceb75e3c08aec5be6c36ac737d5
Chris1429 <97989005+Chris1429@users.noreply.github.com>
2022-03-29 19:29:12 +0100

    Added nonnull UC2 Unit test - db conx error

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,9 +1,25 @@
+import com.napier.sem.usecases.UseCase2;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class UnitTest{
 
+    static UseCase2 useCase2;
+
+    @BeforeAll
+    static void init()
+    {
+        useCase2 = new UseCase2();
+    }
+
     @Test
     void testInitialise() {
 
+    }
+
+    @Test
+    void testUseCase2NonNull()
+    {
+        useCase2.printCountryPopulationReportDESC("Africa");
     }
 }
