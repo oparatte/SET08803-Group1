1eb309fdff2807169c67979535220ea27019d54a
amit169 <amit.lalloo786@gmail.com>
2022-04-25 15:07:59 +0400

    amended App.java to include connection to DB Added world.sql DB  and Docker File Added UseCase1

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,9 +1,12 @@
+import com.napier.sem.usecases.UseCase1;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class UnitTest{
+class UnitTest extends EmbeddedMySql{
 
     @Test
     void testInitialise() {
-
+        UseCase1 useCase1 = new UseCase1();
+        useCase1.printCountryPopulationReportDESC();
     }
 }
